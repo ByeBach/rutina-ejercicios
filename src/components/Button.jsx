874477da4f4146069c758e2663e21eeb3c7c62dd
ByeBach <byebachgm@gmail.com>
2024-10-08 21:45:23 -0300

    card en progreso

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({link}) => {
+
+
+    return(
+        <button><a href={link}>Ver Tutorial</a></button>
+    )
+}
+
+export default Button;
--- src/components/Ejercicios.jsx
+++ src/components/Ejercicios.jsx
@@ -1,16 +1,18 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Ejercicios = ({nombre, repeticiones, cantidad}) => {
 
     return(
         <div className="card">
             <div className="img">
-                
+            <img src="https://i.pinimg.com/564x/85/87/4d/85874dbdafcdcbf57b3b98f6034500cf.jpg" />  
             </div>
             <div className="content"> 
                 <h3>{nombre}</h3>
                 <p>Cantidad de repeticiones: {repeticiones}</p>
                 <p>Cantidad: {cantidad}</p>
+                <Button link="https://www.youtube.com/watch?v=2ZSq1BRYwAI" />
             </div>
         </div>
        
